a4de6ee82f6258e7b2cb67a0053ae862cce38dfe
Potipecan <podpecan.andraz28@gmail.com>
2021-03-22 20:10:19 +0100

    Book copy table class

--- Database/TableClasses/BookCopy.cs
+++ Database/TableClasses/BookCopy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace Database.TableClasses
 {
+    [Table("book_copies")]
     class BookCopy
     {
+        [Column("id"), PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [Column("code"), NotNull, Unique]
+        public string Code { get; set; }
+        [Column("publisher"), NotNull]
+        public string Publisher { get; set; }
+        [Column("year"), NotNull]
+        public int Year { get; set; }
+        [Column("acquisition_type"), NotNull]
+        public int AcquisitionType { get; set; }
+        [Column("acquisition_date"), NotNull]
+        public DateTime AcquisitionDate { get; set; }
+
+        [Column("book_id"), NotNull, ForeignKey(typeof(Book))]
+        public int BookID { get; set; }
+        [ManyToOne]
+        public Book Book { get; set; }
     }
 }
