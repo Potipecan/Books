dd37bbd4c84e62b67abd3e5b18b173155a6b44b0
Potipecan <podpecan.andraz28@gmail.com>
2021-03-22 20:16:47 +0100

    sections table

--- Database/TableClasses/Section.cs
+++ Database/TableClasses/Section.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Database.TableClasses
 {
+    [Table("sections")]
     class Section
     {
+        [Column("id"), PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [Column("name"), NotNull]
+        public string Name { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
     }
 }
