1167ec9d1721de51c1f04de734e06f075050f550
Potipecan <podpecan.andraz28@gmail.com>
2021-03-22 20:13:50 +0100

    Authors books table

--- Database/TableClasses/AuthorBook.cs
+++ Database/TableClasses/AuthorBook.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Database.TableClasses
 {
+    [Table("authors_books")]
     class AuthorBook
     {
+        [Column("author_id"), NotNull, ForeignKey(typeof(Author))]
+        public int AuthorID { get; set; }
+        [Column("book_id"), NotNull, ForeignKey(typeof(Book))]
+        public int BookID { get; set; }
     }
 }
