108d6d53439726fdbfed77076b30c2169bb189bd
Potipecan <podpecan.andraz28@gmail.com>
2021-03-22 21:29:09 +0100

    database manager update

--- Database/DatabaseManager.cs
+++ Database/DatabaseManager.cs
@@ -5,16 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using SQLite;
 using Database.TableClasses;
+using System.IO;
+using System.Diagnostics;
 
 namespace Database
 {
     public class DatabaseManager
     {
+        private const string _databaseFilename = "library.sqlite";
+        private string _path;
+
         private SQLiteAsyncConnection conn;
+        
 
         public DatabaseManager()
         {
-            conn = new SQLiteAsyncConnection("database.sqlite");
+            _path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), _databaseFilename);
+            Debug.WriteLine(_path);
+
+            conn = new SQLiteAsyncConnection(_path, Constants.Flags);
             Task.Run(async () =>
             {
                 await conn.CreateTableAsync<User>();
