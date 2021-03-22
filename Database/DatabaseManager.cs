f2ca9fadeab0be69e99e6dd8dcfea0b3a3cdcf9b
Potipecan <podpecan.andraz28@gmail.com>
2021-03-22 21:04:54 +0100

    database initialization

--- Database/DatabaseManager.cs
+++ Database/DatabaseManager.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using Database.TableClasses;
 
 namespace Database
 {
     public class DatabaseManager
     {
+        private SQLiteAsyncConnection conn;
+
+        public DatabaseManager()
+        {
+            conn = new SQLiteAsyncConnection("database.sqlite");
+            Task.Run(async () =>
+            {
+                await conn.CreateTableAsync<User>();
+                await conn.CreateTableAsync<Book>();
+                await conn.CreateTableAsync<Section>();
+                await conn.CreateTableAsync<Author>();
+                await conn.CreateTableAsync<AuthorBook>();
+                await conn.CreateTableAsync<BookCopy>();
+                await conn.CreateTableAsync<BookRent>();
+                await conn.CreateTableAsync<BookRentRecord>();
+            }).Wait();
+        }
     }
 }
