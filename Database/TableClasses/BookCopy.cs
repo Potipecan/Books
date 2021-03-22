2595091518b710e0cfb0a9a1fad65e442ac62e0a
Potipecan <podpecan.andraz28@gmail.com>
2021-03-22 13:44:01 +0100

    +database classes

--- Database/TableClasses/BookCopy.cs
+++ Database/TableClasses/BookCopy.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Database
+namespace Database.TableClasses
 {
-    public class Class1
+    class BookCopy
     {
     }
 }
---
+++ Database/TableClasses/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Database.TableClasses
+{
+    class User
+    {
+        [AutoIncrement, PrimaryKey]
+        public int ID { get; set; }
+    }
+}
